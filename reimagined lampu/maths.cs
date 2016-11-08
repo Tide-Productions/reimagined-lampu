5c9c3470f8c2e425271c3406feacba75611887ab
kiessler <jonas.kiessler@st.ovgu.de>
2016-11-08 23:29:06 +0100

    Added maths.cs
    wrote a function for converting polar coordinates to cartesian
    coordinates

---
+++ reimagined lampu/maths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace reimagined_lampu
+{
+    class maths
+    {
+
+        public static void polarCoordinates(Vector2 position, Vector2 spawnPosition, double angle, double radius)
+        {
+            position.X = Convert.ToSingle(radius * Math.Cos(angle)) + spawnPosition.X;
+            position.Y = Convert.ToSingle(radius * Math.Sin(angle)) + spawnPosition.Y;
+        }
+
+
+    }
+}
