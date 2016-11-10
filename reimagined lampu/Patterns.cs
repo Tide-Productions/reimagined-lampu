46f58302e200f49e55239202442e27959a06e2ee
Michael Kubicki <kubicki-michael@live.de>
2016-11-10 23:40:41 +0100

    PolarPatterns into Patterns

--- reimagined lampu/Patterns.cs
+++ reimagined lampu/Patterns.cs
@@ -11,6 +11,7 @@ namespace reimagined_lampu
 {
     class Patterns
     {
+       
         int patternStyle;
         Vector2 spawnPosition;
         double radiusChange;
@@ -35,18 +36,85 @@ namespace reimagined_lampu
         /// </summary>
         public void Update()
         {
-            switch (patternStyle)
-            {
-                case 0:
-                    
-                default:
-                    break;
-            }
+            
         }
 
         public void Draw()
         {
 
         }
+    }
+    class PolarPatterns : Patterns
+    {
+        int PatternID;
+        int typeOB;
+        float n;
+        float accelerationOB;
+        float startAngleOS;
+        float angleChangeOB;
+        float startSpeedOB;
+        float angleChangeOS;
+        int timer;
+
+
+
+
+        public PolarPatterns(int PatternID, int typeOB, float numberOB, float accelerationOB, float startAngleOS, float angleChangeOB, float startSpeedOB, float angleChangeOS)
+        {
+            this.PatternID = PatternID;
+            this.typeOB = typeOB;
+            n = numberOB;
+            this.accelerationOB = accelerationOB;
+            this.startAngleOS = startAngleOS;
+            this.angleChangeOB = angleChangeOB;
+            this.startSpeedOB = startSpeedOB;
+            this.angleChangeOS = angleChangeOS;
+            Enemy[] testPattern = new Enemy[n];
+        }
+
+        public PolarPatterns(int PatternID, int typeOB, float numberOB, float accelerationOB, float startAngleOS, float angleChangeOB, float startSpeedOB)
+        {
+            this.PatternID = PatternID;
+            this.typeOB = typeOB;
+            n = numberOB;
+            this.accelerationOB = accelerationOB;
+            this.startAngleOS = startAngleOS;
+            this.angleChangeOB = angleChangeOB;
+            this.startSpeedOB = startSpeedOB;
+            Enemy[] testPattern = new Enemy[n];
+        }
+
+        public new void Update()
+        {
+
+            if (timer % interval == 0)
+            {
+                if (counter < n)
+                {
+                    for (int i = 0; i < anzahl; i++)
+                    {
+                        testPattern[counter + i] = new Enemy(enemyTexture, new Vector2(200, 100), speed, Convert.ToSingle(((counter + i) * Math.PI) / 180));
+                    }
+
+                    counter += anzahl;
+                    //speed = -speed;
+                }
+            }
+
+
+            for (int i = 0; i < counter; i++)
+            {
+                testPattern[i].Update();
+            }
+
+
+
+
+
+            //tick counter
+            timer++;
+
+        }
+
     }
 }
