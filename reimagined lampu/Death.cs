70720d741b5ea8c17de4410ab16f6ec88d0975a8
NinjaProfessor <Kubicki-Michael@live.de>
2017-01-24 17:16:11 +0100

    Added score to death

--- reimagined lampu/Death.cs
+++ reimagined lampu/Death.cs
@@ -24,7 +24,7 @@ namespace reimagined_lampu
         {
             spriteBatch.Draw(texture: background, position: new Vector2(0), scale: new Vector2(GameStuff.Instance.grScale), color: Color.White);
             //spriteBatch.DrawString(GameStuff.Instance.arial, "You have died...", new Vector2(30), Color.White);
-            //spriteBatch.DrawString(GameStuff.Instance.arial, "Press Enter/Space ...", new Vector2(30, 620), Color.White);
+            spriteBatch.DrawString(GameStuff.Instance.arial, "Score: " + GameStuff.Instance.score, new Vector2(30, 620), Color.White);
         }
 
         public void LoadContent(ContentManager Content)
